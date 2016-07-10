4bcea57919dd1d2d13159576d0ccbd5a9df79ef5
Rajalakshmivenkat <rajivenkat.ece@gmail.com>
2016-07-10 12:01:47 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Positive
+{
+public class static void main(String [] args)
+{
+int n;
+Scanner sc=new Scanner(System.in);
+System.out.println("ENTER THE NUMBER");
+if(n==0)
+{
+System.out.println("tHE NUMBER IS ZERO");
+}
+if(n>0)
+{
+System.out.println("tHE NUMBER IS positive");
+}
+else if
+{
+System.out.println("tHE NUMBER IS negative" );
+}
+  }
+}
+
+
